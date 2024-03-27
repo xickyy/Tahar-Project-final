31e14c8cb7a1297f50104119037b638f208d5b17
ricky <ricky_moore@ymail.com>
2024-03-27 14:01:40 -0700

    done with sensors page, just need to add some pictures and style it to work on desktop too

--- frontend/src/components/SensorsPage/index.js
+++ frontend/src/components/SensorsPage/index.js
@@ -2,7 +2,47 @@ import './SensorsPage.css';
 
 function SensorsPage() {
   return (
-    <h2>Sensors Page</h2>
+    <div>
+      <h2 className='text-center font-bold text-2xl mb-4 underline'>Sensors</h2>
+
+      <div className='text-center m-2'>
+        <p className="ml-2 mr-2 mb-4 text-lg" >When it comes to garage door sensors, Tahar Garage Door Services has you covered. Here's why our sensor solutions are the perfect choice for your garage:</p>
+      </div>
+
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Enhanced Safety Features:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Garage door sensors are critical for preventing accidents and injuries. Our advanced sensor technology detects obstructions in the door's path, automatically stopping and reversing the door to avoid collisions. With Tahar Garage Door Services, you can trust that your family and belongings are protected.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Reliable Performance:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Our garage door sensors are designed for consistent, reliable performance, ensuring that your door operates smoothly and safely every time you use it. Whether it's rain, shine, or extreme temperatures, our sensors are up to the task, providing peace of mind year-round.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Easy Installation and Setup:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Installing garage door sensors shouldn't be a hassle. With Tahar Garage Door Services, our experienced technicians will handle the installation process for you, ensuring that your sensors are properly aligned and calibrated for optimal performance. Plus, we'll walk you through the setup process, so you feel confident using your new sensors.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Smart Integration Options:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Want to take your garage door security to the next level? Our sensor solutions can be integrated with smart home systems, allowing you to monitor and control your garage door remotely from your smartphone or other connected devices. With Tahar Garage Door Services, convenience and security go hand in hand.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Expert Troubleshooting and Repair: </p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>If your garage door sensors are malfunctioning or need repair, our team of skilled technicians is here to help. We'll diagnose the issue quickly and accurately, providing prompt repairs to restore your sensors to proper working order. Don't let sensor issues compromise your safety - trust Tahar Garage Door Services to keep your sensors in top condition.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Regular Maintenance Services:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'> Like any other part of your garage door system, sensors require regular maintenance to ensure optimal performance and reliability. Tahar Garage Door Services offers comprehensive maintenance plans to keep your sensors clean, calibrated, and functioning properly, so you can enjoy worry-free operation year after year.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Customer Satisfaction Guaranteed:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>At Tahar Garage Door Services, we're committed to providing exceptional service and customer satisfaction. From our friendly and knowledgeable staff to our top-quality products and services, we go above and beyond to exceed your expectations. Experience the Tahar Garage Door Services difference for yourself - contact us today to learn more about our sensor solutions and schedule an appointment.</p>
+      </div>
+
+      <div>
+        <p className="ml-4 mr-4 mb-4 text-lg">In summary, Tahar Garage Door Services provides top-quality sensors for your garage door needs. With advanced safety features, seamless integration, and expert installation and repair services, we guarantee smooth and secure operation. Trust us for peace of mind and convenience. Contact us today to learn more.</p>
+      </div>
+
+    </div>
   );
 };
 

cbe9965800707f59aa1ecd68c852e41f161511bb
ricky <ricky_moore@ymail.com>
2024-03-27 13:25:31 -0700

    adjusted spacing on services tips

--- frontend/src/components/Tipsntricks/index.js
+++ frontend/src/components/Tipsntricks/index.js
@@ -5,43 +5,43 @@ function Tipsntricks() {
     <div>
       <h2 className='text-center font-bold text-2xl mb-4 underline'>Garage Door Tips & Tricks</h2>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Regular Inspection:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Regular Inspection:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Perform routine checks on your garage door at least once a month. Look for any signs of wear and tear, such as frayed cables, loose hardware, or misalignment.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Lubrication:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Lubrication:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Keep the moving parts of the garage door well-lubricated to ensure smooth operation. Apply a silicone-based lubricant to the rollers, hinges, springs, and tracks.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Tighten Hardware:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Tighten Hardware:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Periodically check and tighten any loose bolts, nuts, or screws on the garage door and its tracks. Vibrations from regular use can cause hardware to loosen over time.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Balance Check:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Balance Check:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Test the balance of your garage door by disconnecting the automatic opener and manually lifting the door halfway. If it doesn't stay in place, it may be out of balance and require adjustment by a professional technician.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Weather Stripping:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Weather Stripping:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Inspect the weather stripping along the bottom of the garage door and replace it if it's worn or damaged. Good weather stripping helps to keep out drafts, debris, and pests.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Clean Tracks:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Clean Tracks:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Regularly clean the tracks to remove any dirt, debris, or obstructions that could affect the smooth movement of the door. Use a damp cloth or brush to wipe away buildup.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Test Safety Features:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Test Safety Features:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Test the safety features of your garage door opener, such as the auto-reverse mechanism and photoelectric sensors, to ensure they are functioning properly.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Spring Maintenance:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Spring Maintenance:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>The springs are a crucial component of the garage door system. Inspect them regularly for signs of wear or damage, and if you notice any issues, have them replaced by a professional technician.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Keep Remote Batteries Fresh:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Keep Remote Batteries Fresh:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Replace the batteries in your garage door remote control regularly to ensure reliable operation.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Professional Maintenance:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Professional Maintenance:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Consider scheduling annual maintenance with a professional garage door technician. They can perform thorough inspections, adjustments, and lubrication to keep your door in top condition.</p>
       </div>
       <p className='text-center text-xl mb-6 mt-4'>By following these maintenance tips, you can help prolong the life of your garage door and ensure it continues to operate safely and smoothly.</p>
--- frontend/src/components/TracksPage/index.js
+++ frontend/src/components/TracksPage/index.js
@@ -12,31 +12,31 @@ function TracksPage() {
             Contact Tahar Garage Door Services today for reliable solutions that prioritize both your safety and the optimal performance of your garage door. Below you can find some additional information about Garage door tracks.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Types:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Types:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>There are generally two types of garage door tracks: vertical tracks and horizontal tracks. Vertical tracks run vertically along the sides of the door opening, while horizontal tracks are installed parallel to the ceiling of the garage.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Components:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Components:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Each track consists of several components, including track sections, brackets, and rollers. The track sections are typically curved to guide the door along its path, and the brackets are used to secure the tracks to the walls and ceiling of the garage.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Installation:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Installation:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Proper installation of garage door tracks is crucial for the smooth operation of the door. Tracks must be securely attached to the walls and ceiling using appropriate fasteners. Additionally, the tracks must be aligned correctly to ensure that the door moves smoothly without binding or rubbing against the tracks.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Maintenance:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Maintenance:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Regular maintenance of garage door tracks is important to keep the door operating smoothly and safely. This includes cleaning the tracks to remove dirt and debris, lubricating the rollers and track surfaces, and checking for any signs of wear or damage.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Safety:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Safety:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Garage door tracks play a significant role in the safe operation of the door. It's essential to ensure that the tracks are in good condition and properly aligned to prevent accidents or injuries.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Clean Tracks:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Clean Tracks:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Regularly clean the tracks to remove any dirt, debris, or obstructions that could affect the smooth movement of the door. Use a damp cloth or brush to wipe away buildup.</p>
       </div>
       <div className='tips-list-el'>
-        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Repairs:</p>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black custom-tips-title border-b-0'>Repairs:</p>
         <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Over time, garage door tracks may become damaged or bent, which can affect the operation of the door. In such cases, repairs may be necessary, such as straightening bent tracks or replacing damaged sections.</p>
       </div>
       <p className='text-center text-xl mb-6 mt-4 ml-2 mr-2'>Overall, garage door tracks are a critical component of the garage door system, and proper installation, maintenance, and repairs are essential for smooth and safe operation.</p>

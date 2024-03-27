bcae5f4fbaa68ccca4d7462084e5a02433c0b569
ricky <ricky_moore@ymail.com>
2024-03-27 13:13:16 -0700

    finished with new services garage door page

--- frontend/src/components/Doorspage/index.js
+++ frontend/src/components/Doorspage/index.js
@@ -2,7 +2,43 @@ import './Doorspage.css';
 
 function Doorspage() {
   return (
-    <h2>Doors Page</h2>
+    <div>
+      <h2 className='text-center font-bold text-2xl mb-4 underline'>Garage Doors</h2>
+      <p className="ml-4 mr-4 mb-4 text-lg">If you're considering upgrading your garage door or need assistance with repairs, you're in the right place. As a leading provider of garage door solutions, we pride ourselves on delivering top-notch service and products tailored to meet your needs. Here are some key points and tips to consider when it comes to garage doors:</p>
+
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Enhanced Curb Appeal:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>A garage door isn't just functional; it's a focal point of your home's exterior. Upgrading to a modern, stylish door can significantly enhance your property's curb appeal, making it more attractive to potential buyers and increasing its overall value.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Safety and Security:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Your garage door serves as a primary entry point to your home. Investing in a high-quality, secure garage door not only protects your vehicles and belongings but also adds an extra layer of security to your property, deterring potential intruders.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Energy Efficiency:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Many modern garage doors are designed with energy efficiency in mind, helping you to reduce heating and cooling costs by minimizing air leakage. Look for insulated doors with weather sealing features to keep your garage comfortable year-round.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Customization Options:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Every home is unique, and your garage door should reflect your personal style. We offer a wide range of customization options, including various materials, colors, and designs, allowing you to create a door that complements your home's architecture and suits your aesthetic preferences.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Professional Installation and Maintenance:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Proper installation is crucial for the optimal performance and longevity of your garage door. Our team of experienced technicians is highly trained to handle installations with precision and care, ensuring that your new door operates smoothly and efficiently for years to come. Additionally, regular maintenance can help prevent costly repairs down the line, so be sure to schedule routine inspections and tune-ups.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Emergency Repairs:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Garage door malfunctions can occur unexpectedly, disrupting your daily routine and compromising your security. That's why we offer prompt emergency repair services to address any issues quickly and efficiently, restoring functionality and peace of mind.</p>
+      </div>
+      <div className='tips-list-el'>
+        <p className='font-bold mr-2 whitespace-nowrap border border-black mb-2 custom-tips-title'>Warranty Coverage:</p>
+        <p className='flex border border-black mb-2 ml-2 mr-2 p-2 custom-tips-info'>Protect your investment with comprehensive warranty coverage. Our products are backed by industry-leading warranties, giving you added confidence and assurance in the quality and durability of your garage door.</p>
+      </div>
+
+      <div>
+        <p className="ml-4 mr-4 mb-4 text-lg">At Tahar Garage Door Services, we're committed to providing exceptional garage door solutions tailored to your needs and budget. Contact us today to learn more about our products and services, and let us help you transform your garage into a safer, more functional, and aesthetically pleasing space.</p>
+      </div>
+    </div>
   );
 };
 
